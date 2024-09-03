1f37e05377a358bc69325136cf73d2609490e1c0
josecarlosgonclves <josecarlosgoncaves@outlook.pt>
2024-09-03 20:27:36 +0100

    done

--- ft_putchar.c
+++ ft_putchar.c
@@ -1,1 +1,18 @@
+// author: José Carlos Gonçalves
+// Date: 02/09/2024
+
 #include <unistd.h>
+
+void ft_putchar(char c);
+
+int main (void)
+{
+	ft_putchar('c');
+	return (0);
+}
+
+void ft_putchar(char c)
+{
+	write (1, &c, 1)
+	write (1 '\n', 1);
+}
